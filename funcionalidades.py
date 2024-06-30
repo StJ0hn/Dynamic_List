6dd99e106c3a72b698b1af242c3a9fc7667b61f0
John Miguel <jonhmiguel138@gmail.com>
2024-06-30 09:42:41 -0300

    troca de programa

--- funcionalidades.py
+++ funcionalidades.py
@@ -1,71 +1,13 @@
 def cabeçalho():
     print(
     '''
-          PLATAFORMA DE CRIAÇÃO DE QUESTIONÁRIOS
+          LISTA DE COMPRAS DINÂMICA
     =================================================
-                       \033[1;34mQuest.Onion\033[m
+                       \033[1;34mList.sys\033[m
     =================================================
     \033[1;33m                    MENU
-    1 - Criar pergunta(s);
-    2 - Responder pergunta(s) já cadastrada(s);
-    3 - Mostrar resposta(s);
+    1 - Adicionar item;
+    2 - Remover item(s);
+    3 - Mostrar item(s);
     4 - Encerrar o programa.\033[m ''')
 
-questoes={}
-
-
-lista_perguntas = []
-def Fazer_pergunta(n_perguntas=None):
-    try:
-        if n_perguntas is None:
-            while True:
-                try:
-                    n_perguntas = int(input("Preencha o campo com um número INTEIRO representado a quantidade de perguntas que deseja elaborar: "))
-                    break
-                except ValueError:
-                    print('Por favor, insira um número inteiro válido')
-
-        for i in range(n_perguntas):
-            pergunta = input(f'Qual a {i+1}° pergunta que você deseja elaborar? ')
-            questoes[f'{i+1}º pergunta'] = pergunta
-
-            lista_perguntas.append(questoes)
-        print("Pergunta(s) elaborada(s) com sucesso.")
-    except KeyboardInterrupt:
-        print('\nPrograma interrompido pelo usuário.')
-
-    return lista_perguntas  
-def responder_perguntas():
-    global lista_perguntas
-    
-    try:
-        if not lista_perguntas:
-            print("Não há perguntas cadastradas para responder.")
-            return []
-
-        respostas = []
-        
-        for i, pergunta_dict in enumerate(lista_perguntas):
-            pergunta = list(pergunta_dict.values())[0]
-            resposta = input(f"Responda à {i+1}ª pergunta: '{pergunta}' ")
-            respostas.append(resposta)
-        print("Respostas registradas com sucesso.")
-    
-    except KeyboardInterrupt:
-        print('\nPrograma interrompido pelo usuário.')
-
-    return respostas
-
-
-def mostrar_respostas(lista_perguntas, respostas):
-    if not respostas:
-        print("Não há respostas para mostrar.")
-    else:
-        print("Respostas fornecidas:")
-        for i, resposta in enumerate(respostas):
-            pergunta = lista_perguntas[i][0]  # Acessa diretamente a pergunta da tupla
-            print(f"Resposta à '{pergunta}': {resposta}")
-
-
-
-respostas = responder_perguntas()
